6d6a17ba8517790662c1ad6321b6a13558a890ed
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-08-11 14:57:00 +0530

    feat: add wishlist route

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import Products from "./pages/Products";
+import Wishlist from "./pages/Wishlist";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar/NavBar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
