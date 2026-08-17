26ff18c875bf07c0e14b69a99805be416b0b3027
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-08-17 13:15:02 +0530

    feat: add mobile sidebar open and close functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,24 @@ import Logo from "./Logo";
 import Navigation from "./Navigation";
 import NavbarIcons from "./NavbarIcons";
 import { IoMenuSharp } from "react-icons/io5";
+import { useState } from "react";
+
 function Navbar() {
+  const [toogle, setToogle] = useState(false);
+  const handleOpenClose = () => {
+    setToogle((prev) => !prev);
+  };
+  console.log(toogle);
+
   return (
     <header className="icons flex justify-between  items-center md:px-8 px-4 py-2 border border-border bg-surface">
       <div className="flex items-center gap-1">
-        <IoMenuSharp className="md:hidden text-lg color" />
+        <button onClick={handleOpenClose}>
+          <IoMenuSharp className="md:hidden text-lg color" />
+        </button>
         <Logo />
       </div>
-      <Navigation />
+      <Navigation toogle={toogle} handleOpenClose={handleOpenClose} />
       <NavbarIcons />
     </header>
   );
--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -1,20 +1,50 @@
 import { NavLink } from "react-router-dom";
-function Navigation() {
+import { IoClose } from "react-icons/io5";
+function Navigation({ toogle, handleOpenClose }) {
   return (
-    <nav className="navbar hidden md:flex ">
-      <ul className=" flex gap-10">
+    <nav
+      className={` fixed top-0 left-0 z-50 h-screen w-64 bg-background p-6 shadow-xl
+    transition-transform duration-300  ${toogle ? "translate-x-0" : "-translate-x-full"} transition duration-300 md:flex md:static md:h-auto md:w-auto md:translate-x-0 md:bg-transparent md:p-0 md:shadow-none`}
+    >
+      <ul className="flex flex-col gap-2 md:flex-row md:gap-6">
+        <button
+          onClick={handleOpenClose}
+          className="mb-6 ml-auto block text-2xl text-text hover:text-primary md:hidden"
+        >
+          <IoClose />
+        </button>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className="block rounded-lg px-4 py-3 font-medium text-text transition hover:bg-orange-50 hover:text-primary"
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/products">Products</NavLink>
+          <NavLink
+            to="/products"
+            className="block rounded-lg px-4 py-3 font-medium text-text transition hover:bg-orange-50 hover:text-primary"
+          >
+            Products
+          </NavLink>
         </li>
 
         <li>
-          <NavLink to="/wishlist">Wishlist</NavLink>
+          <NavLink
+            to="/wishlist"
+            className="block rounded-lg px-4 py-3 font-medium text-text transition hover:bg-orange-50 hover:text-primary"
+          >
+            Wishlist
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/cart">Cart</NavLink>
+          <NavLink
+            to="/cart"
+            className="block rounded-lg px-4 py-3 font-medium text-text transition hover:bg-orange-50 hover:text-primary"
+          >
+            Cart
+          </NavLink>
         </li>
       </ul>
     </nav>
