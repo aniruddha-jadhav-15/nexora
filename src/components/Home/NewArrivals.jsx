47130350c57c2f567a9caf1553ec8474faaf7d3b
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-08-19 20:12:46 +0530

    feat: style New Arrivals section with responsive grid layout

--- src/components/Home/NewArrivals.jsx
+++ src/components/Home/NewArrivals.jsx
@@ -1,11 +1,20 @@
-
-
+import { products } from "../../data/products";
+import ProductCard from "../product/ProductCard";
+import SectionHeader from "./SectionHeader";
 function NewArrivals() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="py-10">
+      <div className="container mx-auto px-4">
+        <SectionHeader header={"New Arrivals"} />
+
+        <div className="mt-6 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} variant="full" />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default NewArrivals
+export default NewArrivals;
