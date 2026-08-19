8d9374cb8c185066b8dd3ffe1a37d0e5c7453bbd
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-08-19 20:13:30 +0530

    feat: add Best Sellers section with responsive layout

--- src/components/Home/BestSellers.jsx
+++ src/components/Home/BestSellers.jsx
@@ -1,5 +1,21 @@
+import { products } from "../../data/products";
+import ProductCard from "../product/ProductCard";
+import SectionHeader from "./SectionHeader";
+
 function BestSellers() {
-  return <div></div>;
+  return (
+    <section className="py-10">
+      <div className="container mx-auto px-4">
+        <SectionHeader header={"Best Sellers"} />
+
+        <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {products.slice(3, 6).map((product) => (
+            <ProductCard key={product.id} product={product} variant={"full"} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default BestSellers;
