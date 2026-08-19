5e2a21b58e9add49b71adf3c515facab5a9bd0a4
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-08-19 20:15:02 +0530

    feat: add reusable SectionHeader component

---
+++ src/components/Home/SectionHeader.jsx
@@ -0,0 +1,9 @@
+function SectionHeader({ header }) {
+  return (
+    <div>
+      <h2 className="text-text font-bold text-h2 my-4 ">{header}</h2>
+    </div>
+  );
+}
+
+export default SectionHeader;
