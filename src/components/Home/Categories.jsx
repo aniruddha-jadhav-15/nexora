95f532485b06dda85006d4563e3a725a80e50791
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-09-06 13:44:40 +0530

    update categories section

--- src/components/Home/Categories.jsx
+++ src/components/Home/Categories.jsx
@@ -1,23 +1,26 @@
-import ProductCard from "../product/ProductCard";
-import SectionHeader from "./SectionHeader";
+import ProductSlider from "../common/ProductSlider";
 
 function Categories({ products }) {
+  let filteredProducts = products;
+  const Categories = filteredProducts.map((product) => product.category.name);
+  const uniqueCategories = [...new Set(Categories)];
+
+  const categoryProducts = uniqueCategories
+    .map((category) => {
+      const product = products.find(
+        (product) => product.category.name === category,
+      );
+
+      return product;
+    })
+    .filter((product) => product !== undefined);
+
   return (
-    <section>
-      <div className="container w-full">
-        <SectionHeader header={"Shop by Categories"} />
-        <div className="flex w-full gap-3 overflow-x-hidden flex-wrap">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="shrink-0 w-[calc(50%-6px)] sm:w-[calc(33.33%-8px)] md:w-[calc(25%-9px)] lg:w-[calc(16.66%-10px)]"
-            >
-              <ProductCard product={product} variant="minimal" />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+    <ProductSlider
+      title="Shop by Categories"
+      products={categoryProducts}
+      variant="minimal"
+    />
   );
 }
 
