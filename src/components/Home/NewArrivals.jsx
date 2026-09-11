5918b4dd4bc07a7cf87050390d91b9fd4227ac06
aniruddha-jadhav-15 <aniruddhajadhav39@gmail.com>
2026-09-11 11:20:15 +0530

    update new arrivals section

--- src/components/Home/NewArrivals.jsx
+++ src/components/Home/NewArrivals.jsx
@@ -1,20 +1,10 @@
-// import { products } from "../../data/products";
-import ProductCard from "../product/ProductCard";
-import SectionHeader from "./SectionHeader";
-function NewArrivals({ products }) {
-  return (
-    <section className="py-10">
-      <div className="container mx-auto px-4">
-        <SectionHeader header={"New Arrivals"} />
+import ProductSlider from "../common/ProductSlider";
 
-        <div className="mt-6 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} variant="full" />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+function NewArrivals({ products }) {
+  let newestProducts = [...products];
+  newestProducts = newestProducts.sort((a, b) => b.id - a.id).slice(10, 21);
+
+  return <ProductSlider title="New Arrivals" products={newestProducts} />;
 }
 
 export default NewArrivals;
